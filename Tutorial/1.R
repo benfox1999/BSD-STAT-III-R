6309b4a03e1c0006107dfc1de807cd514ec5f440
RAJ <148839087+benfox1999@users.noreply.github.com>
2025-08-29 15:49:19 +0530

    Create 1.R for Auto dataset analysis
    Add R script for linear regression analysis on Auto dataset

---
+++ Tutorial/1.R
@@ -0,0 +1,15 @@
+library(ISLR)
+library(ggplot2)
+data(Auto)
+colnames(Auto)
+fit = lm(Auto$acceleration ~ Auto$weight + Auto$horsepower)
+summary(fit)
+plot(Auto$weight, Auto$acceleration)
+fit2 = lm(Auto$acceleration ~ Auto$weight)
+summary(fit2)
+cor(Auto$weight, Auto$acceleration)
+fit2$coefficients
+ggplot(Auto, aes(x=weight, y=acceleration, col=horsepower))+geom_point()+scale_color_gradientn(colors=rainbow(5))
+ind = which(Auto$horsepower>= 40 & Auto$horsepower<=60)
+plot(Auto$weight[ind], Auto$acceleration[ind])
+
